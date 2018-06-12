331e044120209f3d9af861da8eedb668b91126d8
Nevadskiy <nevadskiy@gmail.com>
2018-06-13 00:23:07 +0300

    Fix TestCase for app

--- tests/Integration/LikeTest.php
+++ tests/Integration/LikeTest.php
@@ -4,92 +4,81 @@ namespace Tests\Integration;
 
 use Tests\TestCase;
 use App\Post;
-use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class LikeTest extends TestCase
 {
     use RefreshDatabase;
 
+    protected $post;
+
+    protected $user;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        // Given
+        $this->post = factory(Post::class)->create();
+        $this->signIn();
+    }
+
     /** @test */
     public function a_user_can_like_a_post()
     {
-       // Given:
-       // I have a post and user and user is logged in
-       $post = factory(Post::class)->create();
-       $user = factory(User::class)->create();
-       $this->actingAs($user);
-
-       // When: 
+       // When:
        // He likes a post
-       $post->like();
+       $this->post->like();
 
        // Then:
        // I see evidence in the database and post is liked
        $this->assertDatabaseHas('likes', [
-           'user_id' => $user->id,
-           'likeable_id' => $post->id,
-           'likeable_type' => get_class($post)
+           'user_id' => $this->user->id,
+           'likeable_id' => $this->post->id,
+           'likeable_type' => get_class($this->post)
        ]);
 
-       $this->assertTrue($post->isLiked());
+       $this->assertTrue($this->post->isLiked());
     }
 
     /** @test */
     public function a_user_can_unlike_a_post()
     {
-        // Given:
-        // I have a post and user and user is logged in
-        $post = factory(Post::class)->create();
-        $user = factory(User::class)->create();
-        $this->actingAs($user);
 
         // When:
         // He unlikes a post
-        $post->unlike();
+        $this->post->unlike();
 
         // Then:
         $this->assertDatabaseMissing('likes', [
-            'user_id' => $user->id,
-            'likeable_id' => $post->id,
-            'likeable_type' => get_class($post)
+            'user_id' => $this->user->id,
+            'likeable_id' => $this->post->id,
+            'likeable_type' => get_class($this->post)
         ]);
 
-        $this->assertFalse($post->isLiked());
+        $this->assertFalse($this->post->isLiked());
     }
 
     /** @test */
     public function a_user_may_toggle_a_post_like_status()
     {
-        // Given:
-        // I have a post and user and user is logged in
-        $post = factory(Post::class)->create();
-        $user = factory(User::class)->create();
-        $this->actingAs($user);
-
         // Then
-        $post->toggle();
+        $this->post->toggle();
 
-        $this->assertTrue($post->isLiked());
+        $this->assertTrue($this->post->isLiked());
 
         // And
-        $post->toggle();
+        $this->post->toggle();
 
-        $this->assertFalse($post->isLiked());
+        $this->assertFalse($this->post->isLiked());
     }
 
     /** @test */
     public function a_post_knows_how_many_likes_it_has()
     {
-        // Given:
-        // I have a post and user and user is logged in
-        $post = factory(Post::class)->create();
-        $user = factory(User::class)->create();
 
-        $this->actingAs($user);
+        $this->post->toggle();
 
-        $post->toggle();
-
-        $this->assertEquals(1, $post->likesCount);
+        $this->assertEquals(1, $this->post->likesCount);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,19 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\User;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $user;
+
+    public function signIn($user = null)
+    {
+        $this->user = $user ?: factory(User::class)->create();
+        $this->actingAs($this->user);
+
+        return $this;
+    }
 }
